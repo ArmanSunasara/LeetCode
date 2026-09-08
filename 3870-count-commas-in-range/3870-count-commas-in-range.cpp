9f4a7acb07c63c4739bc5a2168d68ea1987fc810
Arman Sunasara <armansunasara70@gmail.com>
2026-09-08 09:33:10 +0530

    Time: 9 ms (7.75%), Space: 8.6 MB (51.38%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,23 +1,14 @@
 class Solution {
-    void solve(int p ,int n,long long & comma)
-    {
-        if(p<4 || n<1000 ) return;
-        comma++;
-        solve(p,n-1,comma);
-
-    }
 public:
     int countCommas(int n) {
-        int p=0;
-        long long comma=0;
-        int temp =n;
-        while(temp!=0)
-        {
-            p++;
-            temp=temp/10;
-        }
-        solve(p,n,comma);
+        int count = 0;
 
-        return comma;
+        for (int i = 1; i <= n; i++) {
+            if (i >= 1000) {
+                count++;
+            }
+        }
+
+        return count;
     }
 };
